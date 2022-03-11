50589e377a0df63a6f7605265d8d7161f0963d9a
zkirby <zkirby16@gmail.com>
2022-03-10 22:39:26 -0800

    remove old storybook code

--- stories/hoc.jsx
+++ stories/hoc.jsx
@@ -3,20 +3,10 @@ import React from "react";
 import { VesselConnectButton } from "../src";
 
 function App() {
-  const onExit = (error, metadata) => console.log("onExit", error, metadata);
-  const onEvent = (eventName, metadata) =>
-    console.log("onEvent", eventName, metadata);
-  const onSuccess = (token, metadata) =>
-    console.log("onSuccess", token, metadata);
-
   return (
     <VesselConnectButton
       className="CustomButton"
       style={{ padding: "20px", fontSize: "16px", cursor: "pointer" }}
-      token=""
-      onExit={onExit}
-      onSuccess={onSuccess}
-      onEvent={onEvent}
     >
       Open Link and connect your CRM!
     </VesselConnectButton>
